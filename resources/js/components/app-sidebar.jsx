99fd7acff54e717cfc38b5fa1cfed21ef576bb33
kastilyojl <castillojlb0922@gmail.com>
2025-04-02 09:17:48 +0800

    dashboard fixed

--- resources/js/components/app-sidebar.jsx
+++ resources/js/components/app-sidebar.jsx
@@ -566,20 +566,6 @@ export function AppSidebar({ ...props }) {
                               },
                           ],
                       },
-                      {
-                          title: "Curriculum Management",
-                          url: "#",
-                          icon: BookOpen,
-                          items: [
-                              { title: "Program", url: route("admin.program") },
-                              { title: "Subject", url: route("admin.subject") },
-                              {
-                                  title: "Curriculum",
-                                  url: route("admin.curriculum"),
-                              },
-                              { title: "Section", url: route("admin.section") },
-                          ],
-                      },
                   ]
                 : []),
         ];
